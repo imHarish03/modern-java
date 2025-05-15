b6b83357af70f855e15ed50a70685dc31a1d4e90
imHarish03 <harishhari302@gmail.com>
2025-05-15 23:11:06 +0530

    # Revised Predicate

--- src/main/java/func/programming/predicate/PredicateExample2.java
+++ src/main/java/func/programming/predicate/PredicateExample2.java
@@ -1,7 +1,28 @@
 package func.programming.predicate;
 
+import func.programming.Courses;
+import func.programming.Instructor;
+import func.programming.Instructors;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 public class PredicateExample2 {
 	public static void main(String[] args) {
-		
+		Predicate<Instructor> isEnrolled=instructor -> instructor.isEnrolled();
+		Predicate<Instructor> isofferingMoreThan4Courses=instructor -> instructor.getCourses().size()>4;
+
+		Function<String,String> upperCaseFunction=data->data.toUpperCase();
+		BiConsumer<String, List<Courses>> printNameConsumer = (name, course) -> System.out.println(name + " is offering courses on " + course);
+
+		List<Instructor> instructors = Instructors.getAllInstructors();
+
+		instructors.forEach(data -> {
+			if (isEnrolled.and(isofferingMoreThan4Courses).test(data)) {
+				printNameConsumer.accept(upperCaseFunction.apply(data.getName()), data.getCourses());
+			}
+		});
 	}
 }
